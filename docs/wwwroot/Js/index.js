96d70446d9c838669d4e7f639b9213248d5dafd2
SYL <syl8890@qq.com>
2025-12-14 08:05:49 +0800

    1

--- docs/wwwroot/Js/index.js
+++ docs/wwwroot/Js/index.js
@@ -3,7 +3,7 @@ window.$docsify = {
 
     repo: 'true',
     corner: {
-        url: "https://github.com/pl06972515/EDOCS3",
+        url: "https://github.com/pl06972515/EDOCS4",
         icon: "Github", //Github Gitlab Spring 
         //icon: "https://gitee.com/s_origins/ImageStorage/raw/master/PicGo_Typora/20201220213510.png",
         //width: 40,
@@ -22,7 +22,7 @@ window.$docsify = {
     subMaxLevel: 3,
 
     /*... 插件设置 ...*/
-    search: { placeholder: '标题检索', noData: '暂无数据!', depth: 3, namespace: 'EDOCS3', hideOtherSidebarContent: false },
+    search: { placeholder: '标题检索', noData: '暂无数据!', depth: 3, namespace: 'EDOCS4', hideOtherSidebarContent: false },
     copyCode: { buttonText: '点击复制', errorText: 'Error', successText: '已复制到剪贴板' },
     themeable: {
         readyTransition: true,
